6cfa9c95394ad2fe7e143d1674f42d0242eda7b9
Jessa Marie <barrej4@rpi.edu>
2016-10-15 16:40:08 -0400

    changed Directory structure of starter kit, deleted unecessary files, added files

--- core/app.js
+++ core/app.js
@@ -3,19 +3,10 @@
 
     angular.module('app', [
         'ui.router',
-        'app.index',
-        'app.form',
-        'app.json',
-        'app.mountains.details',
-        'app.mountains.list',
-        'app.nav.breadcrumbs',
-        'app.nav.footer',
-        'app.nav.header',
         'app.nav.menu',
-        'app.todos.new',
-        'app.todos.list',
-        'app.directives.datepicker',
-        'app.directives.about',
-        'app.filters'
+        'app.home',
+        'app.projects',
+        'app.about',
+        'app.contact'
     ]);
 })();
