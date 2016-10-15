a4a2f690f41527b79352a499d878e43d958af3ba
Jessa Marie <barrej4@rpi.edu>
2016-10-15 16:40:40 -0400

    changed Directory structure of starter kit, deleted unecessary files, added files

---
+++ app/navigation/navController.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+
+    angular.module('app.nav.menu', ['ui.router'])
+
+            .controller('navController', navController);
+
+    MenuController.$inject = ['$state'];
+
+    function MenuController($state) {
+      
+    }
+})();
